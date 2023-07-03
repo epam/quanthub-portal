87674e576e55296e5cae821fde6d3ec1d18ee2ba
jane-taleika <leyataleya@gmail.com>
2023-07-03 14:22:24 +0200

    add click handler

--- modules/quanthub_core/js/qh_core-power-bi-customizer.js
+++ modules/quanthub_core/js/qh_core-power-bi-customizer.js
@@ -41,7 +41,18 @@ function powerbi_embed_customizeReport($, context, width = 0, height = 0, title 
   const iframes = $(`#${context.selector} iframe`);
   for (let i = 0; i < iframes.length; i++) {
     if ($(iframes[i]).parents().hasClass('ckeditor-accordion-container')){
-      console.log('powerBi is inside ckeditor-accordion-container!')
+      const togglers = $(iframes[i]).parents().find('.ckeditor-accordion-toggler');
+      togglers.each((i)=>{
+        $(togglers[i]).off('click');
+        $(togglers[i]).on('click', function(e){
+          let $title = this.parentNode;
+          if ($title.classList.contains('active')) {
+            //probably will need timeout for animation
+            console.log('powerBi is inside ckeditor-accordion-container!');
+          }
+        });
+      })
+
     }
     iframes[i].frameBorder = 0;
     if (iframes[i].attributes.getNamedItem('style')) {
