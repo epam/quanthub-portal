1a7207f923cf95b3c7a3dd8542d33e6d401552cc
Artem Boiko <Artem Boiko>
2023-08-28 19:07:23 +0300

    Added validation for power bi api route

--- modules/quanthub_core/src/Controller/PowerBIEmbedController.php
+++ modules/quanthub_core/src/Controller/PowerBIEmbedController.php
@@ -2,8 +2,11 @@
 
 namespace Drupal\quanthub_core\Controller;
 
+use Drupal\Component\Uuid\Uuid;
 use Drupal\Core\Controller\ControllerBase;
+use Drupal\Core\Entity\EntityTypeManager;
 use Drupal\quanthub_core\PowerBIEmbedConfigs;
+use GuzzleHttp\Psr7\Response;
 use Laminas\Diactoros\Response\JsonResponse;
 use Symfony\Component\DependencyInjection\ContainerInterface;
 use Symfony\Component\HttpFoundation\Request;
@@ -21,6 +24,13 @@ class PowerBIEmbedController extends ControllerBase {
   protected $powerBIEmbedConfigs;
 
   /**
+   * The EntityTypeManager service.
+   *
+   * @var \Drupal\Core\Entity\EntityTypeManager
+   */
+  protected $entityTypeManager;
+
+  /**
    * Create a PowerBIEmbedController instance.
    *
    * @param \Symfony\Component\DependencyInjection\ContainerInterface $container
@@ -28,7 +38,8 @@ class PowerBIEmbedController extends ControllerBase {
    */
   public static function create(ContainerInterface $container) {
     return new static(
-      $container->get('powerbi_embed_configs')
+      $container->get('powerbi_embed_configs'),
+      $container->get('entity_type.manager')
     );
   }
 
@@ -37,17 +48,37 @@ class PowerBIEmbedController extends ControllerBase {
    *
    * @param \Drupal\quanthub_core\PowerBIEmbedConfigs $powerBIEmbedConfigs
    *   The PowerBIEmbedConfigs object.
+   * @param \Drupal\Core\Entity\EntityTypeManager $entityTypeManager
+   *   The EntityTypeManager service.
    */
-  public function __construct(PowerBIEmbedConfigs $powerBIEmbedConfigs) {
+  public function __construct(PowerBIEmbedConfigs $powerBIEmbedConfigs, EntityTypeManager $entityTypeManager) {
     $this->powerBIEmbedConfigs = $powerBIEmbedConfigs;
+    $this->entityTypeManager = $entityTypeManager;
   }
 
   /**
    * Return Power BI embed configs in the json format.
    */
-  public function postData($reportId, Request $request): JsonResponse {
-    $content = json_decode($request->getContent(), TRUE);
-    return new JsonResponse($this->powerBIEmbedConfigs->getPowerEmbedConfig($reportId, $content["extraDatasets"]));
+  public function postData($reportId, Request $request): JsonResponse | Response {
+    if (Uuid::isValid($reportId)) {
+      $media_storage = $this->entityTypeManager->getStorage('media');
+      $media_ids = $media_storage->getQuery()
+        ->condition('bundle', 'power_bi')
+        ->condition('field_media_power_bi.report_id', $reportId)
+        ->execute();
+
+      try {
+        $content = json_decode($request->getContent(), TRUE, 3, JSON_THROW_ON_ERROR);
+        if (!empty($content['extraDatasets']) && !empty($media_ids)) {
+          return new JsonResponse($this->powerBIEmbedConfigs->getPowerEmbedConfig($reportId, $content['extraDatasets']));
+        }
+      }
+      catch (\Exception) {
+        return new Response(400);
+      }
+    }
+
+    return new Response(400);
   }
 
 }
