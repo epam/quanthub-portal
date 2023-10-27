b11e1635e4b9fc76f95c8f219ecb8d0e397d4d9a
Yauheniya Taleika <Yauheniya_Taleika@epam.com>
2023-10-27 17:56:27 +0200

    header fixes

--- themes/quanthub_tailwindcss/tailwind.config.js
+++ themes/quanthub_tailwindcss/tailwind.config.js
@@ -6,6 +6,7 @@ module.exports = {
         '2xl': {'max': '1919px'},
         'xl': {'max': '1536px'},
         'lg': {'max': '1279px'},
+        'mobile-menu': {'max': '1119px'},
         'md': {'max': '1023px'},
         'sm': {'max': '719px'},
         'xs': {'max': '428px'},
