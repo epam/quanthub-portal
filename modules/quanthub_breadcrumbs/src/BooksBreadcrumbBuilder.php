c8be0b63fb4f83274416e241d0af4eb8bd54e798
Artem Boyko <artemboyko43@gmail.com>
2023-12-28 20:02:57 +0200

    Added translation fucntionality to book breadcrumbs

--- modules/quanthub_breadcrumbs/src/BooksBreadcrumbBuilder.php
+++ modules/quanthub_breadcrumbs/src/BooksBreadcrumbBuilder.php
@@ -6,10 +6,12 @@ use Drupal\Core\Breadcrumb\Breadcrumb;
 use Drupal\Core\Breadcrumb\BreadcrumbBuilderInterface;
 use Drupal\Core\Cache\CacheableDependencyInterface;
 use Drupal\Core\Entity\EntityTypeManager;
+use Drupal\Core\Language\LanguageInterface;
 use Drupal\Core\Link;
 use Drupal\Core\Routing\RouteMatchInterface;
 use Drupal\Core\StringTranslation\StringTranslationTrait;
 use Drupal\node\NodeInterface;
+use Drupal\Core\Language\LanguageManagerInterface;
 
 /**
  * Provides a breadcrumb builder for Books.
@@ -17,6 +19,13 @@ use Drupal\node\NodeInterface;
 class BooksBreadcrumbBuilder implements BreadcrumbBuilderInterface {
 
   use StringTranslationTrait;
+
+  /**
+   * The language manager service.
+   *
+   * @var \Drupal\Core\Language\LanguageManagerInterface
+   */
+  protected $languageManager;
 
   /**
    * Constructs a BooksBreadcrumbBuilder.
@@ -27,8 +36,12 @@ class BooksBreadcrumbBuilder implements BreadcrumbBuilderInterface {
    * @throws \Drupal\Component\Plugin\Exception\InvalidPluginDefinitionException
    * @throws \Drupal\Component\Plugin\Exception\PluginNotFoundException
    */
-  public function __construct(EntityTypeManager $entity_type_manager) {
+  public function __construct(
+    EntityTypeManager $entity_type_manager,
+    LanguageManagerInterface $language_manager
+  ) {
     $this->entityTypeManager = $entity_type_manager->getStorage('node');
+    $this->languageManager = $language_manager;
   }
 
   /**
@@ -48,6 +61,9 @@ class BooksBreadcrumbBuilder implements BreadcrumbBuilderInterface {
    */
   public function build(RouteMatchInterface $route_match): Breadcrumb {
     $breadcrumb = new Breadcrumb();
+    $currentLanguage = $this->languageManager
+      ->getCurrentLanguage(LanguageInterface::TYPE_CONTENT)
+      ->getId();
 
     /** @var \Drupal\node\Entity\Node $node */
     $node = $route_match->getParameter('node');
@@ -58,7 +74,9 @@ class BooksBreadcrumbBuilder implements BreadcrumbBuilderInterface {
       $bookId = $node->book['bid'];
       $bookNode = $this->entityTypeManager->load($bookId);
 
-      $breadcrumb->addLink($bookNode->toLink());
+      if ($bookNode instanceof NodeInterface && $bookNode->hasTranslation($currentLanguage)) {
+        $breadcrumb->addLink($bookNode->getTranslation($currentLanguage)->toLink());
+      }
     }
 
     if ($node->bundle() == 'book_content') {
@@ -70,7 +88,9 @@ class BooksBreadcrumbBuilder implements BreadcrumbBuilderInterface {
       $bookId = $referencedNode->book['bid'];
       $bookNode = $this->entityTypeManager->load($bookId);
       // Book Name item.
-      $breadcrumb->addLink($bookNode->toLink());
+      if ($bookNode instanceof NodeInterface && $bookNode->hasTranslation($currentLanguage)) {
+        $breadcrumb->addLink($bookNode->getTranslation($currentLanguage)->toLink());
+      }
       // Book content node Title item.
       $breadcrumb->addLink(Link::createFromRoute($node->getTitle(), '<none>'));
     }
