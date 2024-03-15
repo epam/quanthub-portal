3f894db5039991bb6d3a3c7f89a8a346194dd953
Illya Havsiyevych <44289086+illya-havsiyevych@users.noreply.github.com>
2024-03-15 17:46:46 +0200

    Update PowerBIEmbedConfigs.php

--- modules/quanthub_core/src/PowerBIEmbedConfigs.php
+++ modules/quanthub_core/src/PowerBIEmbedConfigs.php
@@ -182,7 +182,11 @@ class PowerBIEmbedConfigs {
     ];
 
     if (count($entitledDatasets) > 0) {
-      $payload['identities'] = [['username' => $powerbiUser, 'roles' => [$powerbiRole], 'datasets' => $entitledDatasets]];
+      $payload['identities'] = [[
+        'username' => $powerbiUser, 
+        'roles' => [$powerbiRole],
+        'datasets' => $entitledDatasets
+      ]];
     }
 
     $payload_json = json_encode($payload);
