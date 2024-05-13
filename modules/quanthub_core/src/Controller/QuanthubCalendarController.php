c1d54d5fd97b48eaad056399457cb5c86a25c54a
Oleksandr Tiupa <alextyupa@gmail.com>
2024-05-13 12:15:10 +0300

    fix missed langcode for events URL

--- modules/quanthub_core/src/Controller/QuanthubCalendarController.php
+++ modules/quanthub_core/src/Controller/QuanthubCalendarController.php
@@ -101,9 +101,9 @@ class QuanthubCalendarController extends ControllerBase {
 
       $query->addField('n', 'nid', 'eid');
       $query->addField('n', 'nid', 'id');
-      $query->addField('pa', 'path', 'url');
       $query->addField('nrbd', 'field_rich_brief_descr_value', 'des');
       $query->addExpression("TO_CHAR(to_timestamp(nfrd.field_release_date_value) AT TIME ZONE :timezone, 'YYYY-MM-DD\"T\"HH24:MI:SS')", 'start', [':timezone' => $timezone]);
+      $query->addExpression("CONCAT('/', pa.langcode, pa.alias)", 'url');
       $query->addExpression("TO_CHAR(to_timestamp(nfrd.field_release_date_end_value) AT TIME ZONE :timezone, 'YYYY-MM-DD\"T\"HH24:MI:SS')", 'end', [':timezone' => $timezone]);
       $query->addExpression("FALSE", 'eventDurationEditable');
       $query->addExpression("CASE nfrt.field_release_type_value
@@ -124,22 +124,13 @@ class QuanthubCalendarController extends ControllerBase {
       );
       $data = $query->execute()->fetchAll();
 
+      // Fullcalendar.js need this value as bool.
       foreach ($data as $key => $value) {
-        // Fullcalendar.js need this value as bool.
         if ($data[$key]->allDay == FALSE) {
           $data[$key]->allDay = FALSE;
         }
         else {
           $data[$key]->allDay = TRUE;
-        }
-
-        // This need to generate correct URL with langcode.
-        if (!empty($value->url)) {
-          $url = Url::fromUri("internal:$value->url")
-            ->toString(TRUE)
-            ->getGeneratedUrl();
-
-          $value->url = $url;
         }
       }
 
