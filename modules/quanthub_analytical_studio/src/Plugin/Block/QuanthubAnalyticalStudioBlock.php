7976dfce4af4201e5e11697b1d190c01414ca747
Oleksandr Tiupa <alextyupa@gmail.com>
2024-09-12 16:48:49 +0300

    create Analytical Studio module

---
+++ modules/quanthub_analytical_studio/src/Plugin/Block/QuanthubAnalyticalStudioBlock.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Drupal\quanthub_analytical_studio\Plugin\Block;
+
+use Drupal\Core\Block\BlockBase;
+
+/**
+ * Provides a 'Quanthub Analytical Studio' block.
+ *
+ * @Block(
+ *  id = "quanthub_analytical_studio_block",
+ *  admin_label = @Translation("Quanthub Analytical Studio"),
+ * )
+ */
+class QuanthubAnalyticalStudioBlock extends BlockBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function build() {
+    $build = [];
+    $build['quanthub_analytical_studio_block'] = [
+      '#markup' => '<div id="analytical_studio"></div>',
+      '#attached' => [
+        'library' => 'quanthub_analytical_studio/analytical-studio',
+        'drupalSettings' => [],
+      ],
+    ];
+
+    return $build;
+  }
+
+}
