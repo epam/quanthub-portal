96f0ef8ca12337e6e80e1fdb04140b932a07ff7a
KyryloPopov <kyrylo_popov@epam.com>
2024-09-18 17:51:06 +0300

    Visualization preview implementation

--- modules/quanthub_visualization/src/Plugin/Field/FieldWidget/QuanthubVisualizationWidget.php
+++ modules/quanthub_visualization/src/Plugin/Field/FieldWidget/QuanthubVisualizationWidget.php
@@ -23,12 +23,14 @@ use Drupal\node\Entity\Node;
  *   }
  * )
  */
-class QuanthubVisualizationWidget extends StringTextareaWidget {
+class QuanthubVisualizationWidget extends StringTextareaWidget
+{
 
   /**
    * {@inheritdoc}
    */
-  public function formElement(FieldItemListInterface $items, $delta, array $element, array &$form, FormStateInterface $form_state) {
+  public function formElement(FieldItemListInterface $items, $delta, array $element, array &$form, FormStateInterface $form_state)
+  {
     $element = parent::formElement($items, $delta, $element, $form, $form_state);
 
     $element['#attached']['library'][] = 'quanthub_visualization/dafna-config-editor';
@@ -69,7 +71,7 @@ class QuanthubVisualizationWidget extends StringTextareaWidget {
       '#value' => $this->t('Preview Dataset'),
       '#name' => 'datasetexplorer',
       '#ajax' => [
-        'callback' => [$this, 'preview'],
+        'callback' => [$this, 'datasetExplorerPreview'],
         'wrapper' => 'previewWrapper',
       ],
       '#attributes' => [
@@ -90,8 +92,7 @@ class QuanthubVisualizationWidget extends StringTextareaWidget {
       '#type' => 'html_tag',
       '#tag' => 'div',
       '#attributes' => [
-        'class' => ['dataset_explorer_preview'],
-        'id' => ['dataset_explorer_preview'],
+        'class' => ['datasetexplorer-preview-container'],
       ],
     ];
 
@@ -101,7 +102,8 @@ class QuanthubVisualizationWidget extends StringTextareaWidget {
   /**
    * Ajax callback for preparing visualization data for preview.
    */
-  public function preview(array &$form, FormStateInterface $form_state): AjaxResponse {
+  public function preview(array &$form, FormStateInterface $form_state): AjaxResponse
+  {
     $datasetUrn = Node::load($form_state->getValue('field_qh_visualization_dataset')[0]['target_id'])->get('field_quanthub_urn')->getString();
 //    $form_state->getFormObject()->media->get('field_qh_visualization_dataset')->get(0)->getValue()['target_id']
 
@@ -116,8 +118,7 @@ class QuanthubVisualizationWidget extends StringTextareaWidget {
     $filters = array_filter($filters, function ($item) {
       if (empty($item['componentCode']) && empty($item['operator']) && empty($item['value'])) {
         return FALSE;
-      }
-      else {
+      } else {
         return TRUE;
       }
     });
@@ -158,25 +159,81 @@ class QuanthubVisualizationWidget extends StringTextareaWidget {
       ],
     ];
 
+    $response = new AjaxResponse();
+
+    $response->addCommand(new HtmlCommand('.visualization-preview-container', $preview_data, []));
+    $response->addCommand(new InvokeCommand('html', 'quanthubVisualizationPreview', [0]));
+
+    return $response;
+  }
+
+  /**
+   * @param array $form
+   * @param FormStateInterface $form_state
+   * @return AjaxResponse
+   */
+  public function datasetExplorerPreview(array &$form, FormStateInterface $form_state): AjaxResponse
+  {
+    $datasetUrn = Node::load($form_state->getValue('field_qh_visualization_dataset')[0]['target_id'])->get('field_quanthub_urn')->getString();
+//    $form_state->getFormObject()->media->get('field_qh_visualization_dataset')->get(0)->getValue()['target_id']
+
+//    $datasetUrn = $form_state->getFormObject()->media->get('field_qh_visualization_dataset')->get(0)->get('target_id')->getValue();
+    $filters = $form_state->getValue('field_qh_visualization_filters')[0]['value'];
+//    $filters = reset($form_state->getValue('media'))['fields']['field_qh_visualization_filters'][0]['value'];
+    unset($filters['add_more']);
+    foreach ($filters as &$filter) {
+      unset($filter['_weight']);
+    }
+
+    $filters = array_filter($filters, function ($item) {
+      if (empty($item['componentCode']) && empty($item['operator']) && empty($item['value'])) {
+        return FALSE;
+      } else {
+        return TRUE;
+      }
+    });
+//    $displayConfig = reset($form_state->getValue('media'))['fields']['field_qh_visualization_transform'][0]['value'];
+    $displayConfig = $form_state->getValue('field_qh_visualization_transform')[0]['value'];
+    $dataTransformationsConfig['display'] = [];
+    // Build dataTransformationsConfig.
+    foreach ($displayConfig as $item) {
+      if (is_array($item) && !empty($item['dimensionId'])) {
+        $dataTransformationsConfig['display'][] = [
+          'dimensionId' => $item['dimensionId'],
+          'field' => $item['field'],
+        ];
+      }
+    }
+//    $visualizationConfig = reset($form_state->getValue('media'))['fields']['field_media_qh_visualization'][0]['value'];
+//    $data = \Drupal::service('quanthub_visualization.media_config')->extractMediaConfig($form_state);
+    $visualizationConfig = $form_state->getValue('field_media_qh_visualization')[0]['value'];
+    $dafnaType = $form_state->getValue('field_qh_visualization_type')[0]['value'];
+    $dataFilters = $form_state->getValue('field_visualization_filters')[0]['value'];
+    $preview_data['#attached']['drupalSettings']['workspaceId'] = getenv('SDMX_WORKSPACE_ID');
+    $preview_data['#attached']['drupalSettings']['quanthubVisualization']['media'][0] = [
+      'dafnaType' => $dafnaType,
+      'dataFilters' => empty($dataFilters) ? null : $dataFilters,
+      'visualizationTitle' => $form_state->getValue('name')[0]['value'],
+      'visualizationConfig' => json_decode($visualizationConfig),
+      'dataTransformationsConfig' => $dataTransformationsConfig,
+      'dataSourceConfig' => ['timeFilters' => $filters], //filters
+      'dataSource' => $datasetUrn,
+    ];
+
     $preview_data['qh_datasetexplorer_preview'] = [
       '#type' => 'html_tag',
       '#tag' => 'div',
       '#attributes' => [
-        'class' => 'dataset_explorer_preview',
-        'id' => 'dataset_explorer_preview',
+        'class' => 'dataset_explorer_preview dataset_explorer',
+        'id' => 'dataset_explorer_preview dataset_explorer',
         'data-media-id' => 0,
       ],
     ];
 
     $response = new AjaxResponse();
-    if ($form_state->getTriggeringElement()['#name'] == 'datasetexplorer') {
-      $response->addCommand(new HtmlCommand('.datasetexplorer-preview-container', $preview_data, []));
-      $response->addCommand(new InvokeCommand('html', 'quanthubDatasetExplorerPreview', [0]));
-    } else {
-      $response->addCommand(new HtmlCommand('.visualization-preview-container', $preview_data, []));
-      $response->addCommand(new InvokeCommand('html', 'quanthubVisualizationPreview', [0]));
-    }
 
+    $response->addCommand(new HtmlCommand('.datasetexplorer-preview-container', $preview_data, []));
+    $response->addCommand(new InvokeCommand('html', 'quanthubDatasetExplorerPreview', [0]));
     return $response;
   }
 
