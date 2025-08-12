7d04bd8aa623083eb0cc902e3e6ff16e4dbb4f75
Serhii Herasymov <serhii_herasymov@epam.com>
2025-08-12 10:43:41 +0300

    Update tailwind.

--- themes/quanthub_tailwindcss/postcss.config.js
+++ themes/quanthub_tailwindcss/postcss.config.js
@@ -1,8 +1,4 @@
-const tailwindcss = require('@tailwindcss/postcss');
-
+const tailwindcss = require('tailwindcss');
 module.exports = {
-  plugins: [
-    'postcss-preset-env',
-    tailwindcss,
-  ],
+  plugins: ['postcss-preset-env', tailwindcss],
 };
