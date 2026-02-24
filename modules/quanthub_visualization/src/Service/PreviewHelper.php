c0b727c5df910d691a3dda4b830ecce93a8a9edb
Oleksandr Tiupa <alextyupa@gmail.com>
2026-02-24 13:31:49 +0200

    changed the dataset field name for medias

--- modules/quanthub_visualization/src/Service/PreviewHelper.php
+++ modules/quanthub_visualization/src/Service/PreviewHelper.php
@@ -30,7 +30,7 @@ class PreviewHelper {
    */
   public function provideDrupalSettingsData(FormStateInterface $form_state) {
     $nodeStorage = $this->entityTypeManager->getStorage('node');
-    $datasetUrn = $nodeStorage->load($form_state->getValue('field_qh_visualization_dataset')[0]['target_id'])->get('field_quanthub_urn')->getString();
+    $datasetUrn = $nodeStorage->load($form_state->getValue('field_dataset')[0]['target_id'])->get('field_quanthub_urn')->getString();
 
     $filters = $form_state->getValue('field_qh_visualization_filters')[0]['value'];
 
